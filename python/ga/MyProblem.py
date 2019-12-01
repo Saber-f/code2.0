32f1eac457b9e49541a668169c6609d93c4a27a7
dt <saber-f>
2019-12-01 21:36:24 +0800

    u

--- python/ga/MyProblem.py
+++ python/ga/MyProblem.py
@@ -1,7 +1,6 @@
 import numpy as np
 import geatpy as ea
 import networkx as nx
-import matplotlib.pyplot as plt
 
 class MyProblem(ea.Problem):              # 继承Problem父类
     def getD(self,node_list,edge_list):
